a609d635415475bd9b805859be9863fb9b345d57
bnastov <bnastov@NASTOV-PC.personnel.mines-ales.fr>
2015-06-04 16:54:20 +0200

    les message sont envoyé à partir de devis.php et contact.php 26/03/2014

--- index.php
+++ index.php
@@ -10,8 +10,41 @@ and open the template in the editor.
     </head>
     <body>
         HELLO WORLD
+
+        <a class="twitter-timeline" href="https://twitter.com/NastovB" data-widget-id="606449930329333760">Tweets by @NastovB</a>
+        <script>!function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = p + "://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, "script", "twitter-wjs");</script>
+
+        <a class="twitter-timeline"
+           data-widget-id="601877449689804800"
+           href="https://twitter.com/TwitterMusic/timelines/393773266801659904"
+           data-custom-timeline-id="393773266801659904">
+            Music Superstars
+        </a>
+        <script>
+            twttr.ready(function(twttr) {
+                twttr.widgets.createTimeline(
+                        "601877449689804800",
+                        document.getElementById("container"),
+                        {
+                            customTimelineId: "393773266801659904"
+                        }
+                );
+            });
+        </script>
+        
+        <blockquote class="twitter-tweet" lang="en"><p lang="sl" dir="ltr">tvit</p>&mdash; Blazo (@NastovB) <a href="https://twitter.com/NastovB/status/602414658197270528">May 24, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
         <?php
-        // put your code here
+// put your code here
         ?>
     </body>
 </html>
